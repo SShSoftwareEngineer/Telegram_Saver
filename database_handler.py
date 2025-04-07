f48bfc49dffb8c19ba6c43d3ec07baf561cc5bfe
SShSoftwareEngineer <shypulin@ukr.net>
2025-04-07 23:39:19 +0300

    Added a tag table to the DB Optimized the DB structure Corresponding changes have been made to the model

--- database_handler.py
+++ database_handler.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import List
 
 from sqlalchemy import create_engine, Integer, ForeignKey, Text, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, Session
@@ -10,7 +11,9 @@ TABLE_NAMES = {'message': 'messages',
                'dialog': 'dialogs',
                'group': 'groups',
                'file': 'files',
-               'file_type': 'file_types'}
+               'file_type': 'file_types',
+               'tag': 'tags',
+               'tag_message': 'tags_messages'}
 
 
 class Base(DeclarativeBase):
@@ -20,41 +23,46 @@ class Base(DeclarativeBase):
 class Message(Base):
     __tablename__ = TABLE_NAMES['message']
     id: Mapped[int] = mapped_column(primary_key=True)
-    dialog_id: Mapped[int] = mapped_column(Integer, ForeignKey(f'{TABLE_NAMES['dialog']}.message_id'))
-    group_id: Mapped[int] = mapped_column(Integer, ForeignKey(f'{TABLE_NAMES['group']}.message_id'))
-    file_id: Mapped[int] = mapped_column(Integer, ForeignKey(f'{TABLE_NAMES['file']}.message_id'))
+    dialog_id: Mapped[int] = mapped_column(Integer, ForeignKey(f'{TABLE_NAMES['dialog']}.dialog_id'))
+    group_id: Mapped[int] = mapped_column(Integer, ForeignKey(f'{TABLE_NAMES['group']}.group_id'), nullable=True)
+    file_id: Mapped[int] = mapped_column(Integer, ForeignKey(f'{TABLE_NAMES['file']}.file_id'), nullable=True)
     message_id: Mapped[int] = mapped_column(Integer, unique=True, index=True)
     date_time: Mapped[datetime]
-    from_id: Mapped[int] = mapped_column(Integer)
+    from_user: Mapped[str] = mapped_column(Text, nullable=True)
     text: Mapped[str] = mapped_column(Text, nullable=True)
     raw_text: Mapped[str] = mapped_column(Text, nullable=True)
+    tag_messages: Mapped[List['TagMessage']] = relationship('TagMessage', back_populates='message')
+
+    @property
+    def tags(self):
+        return [tm.tag for tm in self.tag_messages]
 
 
 class Dialog(Base):
     __tablename__ = TABLE_NAMES['dialog']
     id: Mapped[int] = mapped_column(primary_key=True)
-    dialog_id: Mapped[int] = mapped_column(Integer)
+    dialog_id: Mapped[int] = mapped_column(Integer, index=True)
     dialog_title: Mapped[str] = mapped_column(Text, nullable=True)
-    message: Mapped["Message"] = relationship(back_populates="dialog")
+    message_id: Mapped["Message"] = relationship(back_populates="dialog_id")
 
 
 class Group(Base):
     __tablename__ = TABLE_NAMES['group']
     id: Mapped[int] = mapped_column(primary_key=True)
-    group_id: Mapped[int] = mapped_column(Integer)
-    message: Mapped["Message"] = relationship(back_populates="group")
+    group_id: Mapped[int] = mapped_column(Integer, index=True)
+    message_id: Mapped["Message"] = relationship(back_populates="group_id")
 
 
 class File(Base):
     __tablename__ = TABLE_NAMES['file']
     id: Mapped[int] = mapped_column(primary_key=True)
-    file_id: Mapped[int] = mapped_column(Integer)
+    file_id: Mapped[int] = mapped_column(Integer, index=True)
     file_path: Mapped[str] = mapped_column(Text)
     file_name: Mapped[str] = mapped_column(Text)
     type_id: Mapped[int] = mapped_column(Integer, ForeignKey(f'{TABLE_NAMES['file_type']}.id'))
     size: Mapped[int] = mapped_column(Integer)
     thumbnail: Mapped[str] = mapped_column(Text)
-    message: Mapped["Message"] = relationship(back_populates="file_id")
+    message_id: Mapped["Message"] = relationship(back_populates="file_id")
 
 
 class FileType(Base):
@@ -62,6 +70,25 @@ class FileType(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     type_name: Mapped[str] = mapped_column(Text)
     file: Mapped["File"] = relationship(back_populates="type_id")
+
+
+class Tag(Base):
+    __tablename__ = TABLE_NAMES['tag']
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tag_name: Mapped[str] = mapped_column(Text)
+    tag_messages: Mapped[List['TagMessage']] = relationship('TagMessage', back_populates='tag')
+
+    @property
+    def messages(self):
+        return [tm.message for tm in self.tag_messages]
+
+
+class TagMessage(Base):
+    __tablename__ = TABLE_NAMES['tag_message']
+    tag_id: Mapped[int] = mapped_column(Integer, ForeignKey(f'{TABLE_NAMES['tag']}.id'), primary_key=True)
+    message_id: Mapped[int] = mapped_column(Integer, ForeignKey(f'{TABLE_NAMES['message']}.id'), primary_key=True)
+    tag: Mapped['Tag'] = relationship('Tag', back_populates='tag_messages')
+    message: Mapped['Message'] = relationship('Message', back_populates='tag_messages')
 
 
 # Connecting to the database. Creating a database connection and session
